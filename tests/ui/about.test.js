878c1592e9310f0f170fb867ff5f7651e8a5d5bb
Miroslav Trendafilov <120394438+MiroslavTrendafilovv@users.noreply.github.com>
2023-08-12 17:46:56 +0300

    Update about.test.js

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('Check about page', async ({ page }) => {
   await page.goto('https://miroslav1996.onrender.com/about');
-  const heading = await page.$('h2');
+  const heading = await page.$('h1');
   const text = await heading.textContent();
   expect(text).toBe('About');
 });
