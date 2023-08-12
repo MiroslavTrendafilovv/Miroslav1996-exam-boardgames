ab4a9f6f8096791b34e896014e663f73a021fa76
Miroslav Trendafilov <120394438+MiroslavTrendafilovv@users.noreply.github.com>
2023-08-12 17:55:44 +0300

    Update boardgames.test.js

--- tests/ui/boardgames.test.js
+++ tests/ui/boardgames.test.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test');
 test('Check boardgames page', async ({ page }) => {
     await page.goto('https://miroslav1996.onrender.com/boardgames');
     const list = await page.$('ul');
-    expect(received).toBeTruthy();
+    expect(list).toBeTruthy();
   });
 
 
